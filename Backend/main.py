0246174ea92ea1a6de7789a8a24de9300a00ce20
KaiyWhiteSnow <106684837+KaiyWhiteSnow@users.noreply.github.com>
2023-10-13 14:07:44 +0200

    BE: added root endpoint for testing

--- Backend/main.py
+++ Backend/main.py
@@ -3,7 +3,10 @@ from flask import jsonify, Flask, session
 api = Flask(__name__)
 api.secret_key = "Praeteritum"
 
-session_instance = session()
+@api.route("/", methods=["GET", "POST"])
+def root():
+    testEndpoint = ["hello", "world!"]
+    return jsonify(testEndpoint)
 
 if __name__ == "__main__":
     api.run(host="0.0.0.0", port=8002)
---
+++ build.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+cd ./Backend
+
+python3 main.py
