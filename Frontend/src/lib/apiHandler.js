1ee9677668b9e4317b82cf95239ceab1ee48ab52
Jan Palma <mobilex1122.en@gmail.com>
2023-10-13 13:51:39 +0000

    Created basic api Framework
    Added uniFetch function that fatches any url that's specified.
    More info can be get using jsdoc

---
+++ Frontend/src/lib/apiHandler.js
@@ -0,0 +1,61 @@
+import {
+	PUBLIC_API_URL,
+} from '$env/static/public'
+
+export class EndpointFormatError extends Error {
+	/**
+	 * 
+	 * @param {string} message Error message
+	 */
+	constructor(message) {
+		super(message); // (1)
+		this.name = "ValidationError"; // (2)
+	}
+}
+
+export default class APIHandler {
+	/**
+	 * @description Api Request Handler
+	 */
+	constructor() {
+
+		if (!PUBLIC_API_URL.endsWith("/")) {
+			this.url = PUBLIC_API_URL + "/"
+		} else {
+			this.url = PUBLIC_API_URL
+		}
+
+
+
+	}
+	/**
+	 * @description Universal api request (Not recommended for production)
+	 * @param {string} url Endpoint url (without api url)
+	 * @returns {Promise<Object>} retu
+	 */
+	async uniFetch(url) {
+		try {
+			const responceRaw = await fetch(this.url + this.endpointFormat(url))
+			const responce = await responceRaw.json()
+			return responce
+		} catch (error) {
+			throw new Error("Fetch error: " + error)
+		}
+	}
+	/**
+	 * @description Checks and formats endpoint url.
+	 * @throws {Error} Errors out if
+	 * @param {string} endpoint Unformated endpoint
+	 * @returns {string} Formated endpoint 
+	 */
+	endpointFormat(endpoint) {
+		if (endpoint.endsWith("/")) {
+			endpoint = endpoint.slice(0, -1)
+
+		}
+		if (endpoint.startsWith("/")) {
+			endpoint = endpoint.slice(1)
+		}
+		return endpoint
+	}
+}
