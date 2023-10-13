d77f0c3399a67d95c2e9c2ca827a825378bf9ce1
Jan Palma <60009551+mobilex1122@users.noreply.github.com>
2023-10-13 18:42:12 +0200

    Added login page (base)

---
+++ Frontend/src/routes/login/+page.svelte
@@ -0,0 +1,17 @@
+<div class="flex flex-col md:flex-row h-screen">
+	<div class="bg-gray-500 flex-grow">
+		<img src="/image/bg/login-2x.jpg" class="w-full h-full min-h-0 object-cover" alt="">
+	</div>
+	<div class="p-4 pb-20 md:px-20 flex flex-col gap-2 shadow-2xl md:justify-center">
+		<img src="" class="mx-auto bg-slate-800 w-20 aspect-square" alt="">
+		<h1 class="text-xl text-center">Login</h1>
+		<input class="bg-gray-400 w-full md:w-80 p-2 rounded-xl" type="email">
+		<input class="bg-gray-400 w-full md:w-80 p-2 rounded-xl" type="password">
+		<button class="bg-gray-400 w-full p-2 rounded-xl">Login</button>
+	
+	</div>
+</div>
+
+
+<style lang="postcss">
+</style>
