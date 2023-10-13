faed50aa396e0e34fd746771641026f807e4b0f1
KaiyWhiteSnow <106684837+KaiyWhiteSnow@users.noreply.github.com>
2023-10-13 18:04:36 +0200

    BE: Template for kristi

--- Backend/models.py
+++ Backend/models.py
@@ -3,3 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
 Base = declarative_base()
+
+class user(Base):
+    __tablename__ = "userModel"
+
+    uID = Column(Integer, nullable=False, unique=True, primary_key=True)
