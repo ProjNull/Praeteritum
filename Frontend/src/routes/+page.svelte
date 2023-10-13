8cdbc1e3a28e7e23be0d0cb43aa63aa222b55127
Jan Palma <60009551+mobilex1122@users.noreply.github.com>
2023-10-13 18:42:00 +0200

    Added loading

--- Frontend/src/routes/+page.svelte
+++ Frontend/src/routes/+page.svelte
@@ -6,7 +6,33 @@
 		reponse = await api.uniFetch("/ping/")
 		console.log(reponse);
 	})()
+
+
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> {reponse}</p>
+<div class="loading">
+	<div class="w-12 h-12 border-black border-4 border-l-transparent rounded-[50%]  box-border"></div>
+</div>
+
+
+<style lang="postcss">
+	.loading div {
+		animation: loading 2s cubic-bezier(.62,.28,.41,.77) infinite;
+		position: fixed;
+		top: 50%;
+		left: 50%;
+		opacity: 1;
+	}
+	@keyframes loading {
+		0% {
+			transform: translate(-50%,-53%) rotate(0turn);
+
+		}
+		100% {
+			transform: translate(-50%,-50%) rotate(5turn);
+		}
+	}
+	.loading.hide {
+		opacity: 0;
+	}
+</style>
