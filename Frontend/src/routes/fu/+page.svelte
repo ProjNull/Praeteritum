024470b195e3daa79167a5ff3b47f5d3ed4d09f1
Jan Palma <60009551+mobilex1122@users.noreply.github.com>
2023-10-13 22:44:34 +0200

    Create +page.svelte

---
+++ Frontend/src/routes/fu/+page.svelte
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+	import { io } from "socket.io-client";
+	var socket = io('http://172.21.112.249:8002');
+
+	socket.on('connect', function () {
+		socket.emit('my_event', {data:"Hello"});
+	});
+
+	socket.on('my_response', function (data) {
+		console.log(data.data);
+	});
+</script>
