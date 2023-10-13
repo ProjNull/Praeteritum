2569479abb1621d64a57b7682368646f3e684766
Jan Palma <60009551+mobilex1122@users.noreply.github.com>
2023-10-13 22:44:03 +0200

    Added fonts from google fonts

--- Frontend/src/routes/+layout.svelte
+++ Frontend/src/routes/+layout.svelte
@@ -3,3 +3,12 @@
 </script>
 
 <slot />
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap');
+
+	:global(h1.project-name) {
+		font-family: 'Oswald', sans-serif;
+		font-weight: 800;
+	}
+</style>
