0a4fc7f5740cbaf649b157690172e2abedfabd92
Jan Palma <60009551+mobilex1122@users.noreply.github.com>
2023-10-13 19:40:53 +0200

    Added login and registration
    Added basic input validation for registration

--- Frontend/src/routes/login/+page.svelte
+++ Frontend/src/routes/login/+page.svelte
@@ -1,17 +1,21 @@
 <div class="flex flex-col md:flex-row h-screen">
-	<div class="bg-gray-500 flex-grow">
+	<div class="bg-gray-500 flex-grow relative">
 		<img src="/image/bg/login-2x.jpg" class="w-full h-full min-h-0 object-cover" alt="">
+		<span class="absolute bottom-1 right-1 text-white">&copy; Pexels</span>
 	</div>
 	<div class="p-4 pb-20 md:px-20 flex flex-col gap-2 shadow-2xl md:justify-center">
 		<img src="" class="mx-auto bg-slate-800 w-20 aspect-square" alt="">
-		<h1 class="text-xl text-center">Login</h1>
-		<input class="bg-gray-400 w-full md:w-80 p-2 rounded-xl" type="email">
-		<input class="bg-gray-400 w-full md:w-80 p-2 rounded-xl" type="password">
+		<h1 class="text-xl text-center">Praeterium</h1>
+		<input class="login-input" required type="email" placeholder="Email">
+		<input class="login-input" required minlength="8" type="password" placeholder="Password">
 		<button class="bg-gray-400 w-full p-2 rounded-xl">Login</button>
-	
+		<a href="/register" class="text-gray-800 underline mx-auto">Don't have an account?</a>
 	</div>
 </div>
 
 
 <style lang="postcss">
+	.login-input {
+		@apply bg-gray-200 w-full min-w-[15rem] p-2 duration-500 rounded-xl border-2 transition-colors focus:outline-none valid:bg-green-100 valid:border-green-600
+	}
 </style>
---
+++ Frontend/src/routes/register/+page.svelte
@@ -0,0 +1,54 @@
+<script>
+// @ts-nocheck
+
+	let firstClick = true
+	let passMatch = true
+
+	let pass = ["",""]
+	const submit = (e) => {
+		const formData = new FormData(e.target);
+		firstClick = false
+	}
+	const checkPassMatch = () => {
+		passMatch = (pass[0] == pass[1])
+	}
+</script>
+
+<div class="flex flex-col sm:flex-row h-screen">
+	<div class="bg-gray-500 flex-grow relative">
+		<img src="/image/bg/login-2x.jpg" class="w-full h-full min-h-0 object-cover" alt="">
+		<span class="absolute bottom-1 right-1 text-white">&copy; Pexels</span>
+	</div>
+	<form class="p-4 pb-20 sm:px-20 flex flex-col gap-2 shadow-2xl sm:justify-center" on:submit|preventDefault={submit}>
+		<img src="" class="mx-auto bg-slate-800 w-20 aspect-square" alt="">
+		<h1 class="text-xl text-center">Praeterium</h1>
+		<div class="flex flex-col sm:flex-row gap-2">
+			<input class="login-input-flex {!firstClick ? "validate": ""}" required type="text" placeholder="First Name">
+			<input class="login-input-flex {!firstClick ? "validate": ""}" required type="text" placeholder="Last Name">
+		</div>
+		<input class="login-input {!firstClick ? "validate": ""}" required type="email" placeholder="Email">
+		<input on:keydown={(e) => e.target.classList.add("validate")} bind:value={pass[0]} on:keyup={checkPassMatch} class="login-input" required minlength="8" placeholder="Password" type="password">
+		<input on:keydown={(e) => e.target.classList.add("validate")} bind:value={pass[1]} on:keyup={checkPassMatch} class="login-input {!passMatch ? "invalid": ""}" required minlength="8" placeholder="Re-Password" type="password">
+		<input on:click={() => firstClick = false} class="bg-gray-200 rounded-xl p-2 hover:bg-gray-300 focus:bg-gray-300" type="submit" value="Register">
+		<a href="/login" class="text-gray-800 underline mx-auto">Have an account?</a>
+	</form>
+</div>
+
+
+<style lang="postcss">
+	input:focus {
+		@apply outline outline-offset-2 outline-pink-700
+	}
+	.login-input {
+		@apply bg-gray-200 w-full min-w-[15rem] p-2 duration-200 rounded-xl border-2 transition-colors valid:bg-green-100 valid:border-green-600
+	}
+	.login-input-flex {
+		@apply bg-gray-200 w-full flex-1 p-2 duration-500 rounded-xl border-2 transition-colors valid:bg-green-100 valid:border-green-600
+	}
+	input.validate {
+		@apply invalid:bg-red-200 invalid:border-red-700
+	}
+	input.invalid {
+		@apply !bg-red-200 !border-red-700
+	}
+</style>
