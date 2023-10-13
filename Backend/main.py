8c7c84e1af98825afff26cd6749c80b44462e683
Stefi Prokop <hyscript7@seznam.cz>
2023-10-14 01:32:40 +0200

    Refactor

--- Backend/main.py
+++ Backend/main.py
@@ -1,20 +1,14 @@
 """ May the spaghetti begin """
-
-
-from flask import jsonify, Flask, request, session, json
-from flask_socketio import SocketIO
-
-from database import func
-from database import Session
-from models import Users
-
-from werkzeug.security import generate_password_hash, check_password_hash
-
+from datetime import datetime, timedelta
 from functools import wraps
 
-from datetime import datetime, timedelta
-
 import jwt
+from database import Session, func
+from flask import Flask, json, jsonify, request, session
+from flask_socketio import SocketIO
+from models import Users
+from werkzeug.exceptions import HTTPException
+from werkzeug.security import check_password_hash, generate_password_hash
 
 SECRET_KEY = "Praeteritum-CHANGE-ME-TO-SOMETHING-RANDOM"
 
@@ -22,29 +16,9 @@ api = Flask(__name__)
 api.secret_key = SECRET_KEY
 
 socketio = SocketIO(api)
-
 socketio.init_app(api, cors_allowed_origins="*")
 
 session_instance = Session()
-
-
-def commit_data():
-    """
-    Commit sample user data to the database.
-    """
-    name = "pes"
-    email = "email@email.email"
-    password = "passwordE"
-
-    usr = Users(Name=name, Password=password, Email=email)
-
-    session_instance.add(usr)
-    session_instance.commit()
-
-
-commit_data()
-
-from werkzeug.exceptions import HTTPException
 
 
 @api.errorhandler(HTTPException)
@@ -54,9 +28,7 @@ def handle_exception(e):
 
     :param e: The HTTPException to handle.
     """
-    # start with the correct headers and status code from the error
     response = e.get_response()
-    # replace the body with JSON
     response.data = json.dumps(
         {
             "code": e.code,
@@ -87,8 +59,8 @@ def generate_jwt(payload):
     :return: The generated JWT token.
     """
     try:
-        payload["exp"] = datetime.utcnow() + timedelta(days=1) 
-        payload["iat"] = datetime.utcnow() 
+        payload["exp"] = datetime.utcnow() + timedelta(days=1)
+        payload["iat"] = datetime.utcnow()
 
         token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
         return token
@@ -128,13 +100,15 @@ def generateToken(ID):
 
 
 # decorator for verifying the JWT
-def token_required(f):
+def requires_authorization(f):
     """
     Decorator function for verifying JWT tokens.
+    Requires your function to have a variable of type Users as the first argument.
 
     :param f: The function to decorate.
     :return: The decorated function.
     """
+
     @wraps(f)
     def decorated(*args, **kwargs):
         token = None
@@ -149,9 +123,10 @@ def token_required(f):
             # decoding the payload to fetch the stored details
             data = verify_jwt(token)
             current_user = (
-                session_instance.query(Users).filter_by(User_ID=data["User_ID"]).first()
+                session_instance.query(Users).filter_by(
+                    User_ID=data["User_ID"]).first()
             )
-        except Exception as e:
+        except:
             return (
                 jsonify({"message": "Token evaluation unsuccessfull"}),
                 401,
@@ -170,23 +145,38 @@ def register():
     :return: JSON response indicating the registration status.
     """
     if request.method == "POST":
-        username = request.form.get("username")
+        name = request.form.get("displayname")
         password = request.form.get("password")
         email = request.form.get("email")
 
-        if username and password:
+        if email and password:
+            if not name:
+                name = email.split("@")[0]
             q = session_instance.query(Users).filter_by(Email=email).first()
             if not q:
-                usr = Users(Name=username, Password=generate_password_hash(password), Email=email)
+                usr = Users(
+                    Name=name,
+                    Password=generate_password_hash(password),
+                    Email=email,
+                )
                 session_instance.add(usr)
                 session_instance.commit()
 
-                return jsonify("Data is now in DB!")
-            return jsonify("Data does not match")
+                return jsonify(
+                    {"User_ID": usr.User_ID, "message": "Registration successfull!"}
+                )
+            return jsonify({"message": "Email already taken!"})
         else:
-            return jsonify("Please fill out all fields.")
+            return jsonify(
+                {
+                    "message": "Missing one or more of the required fields!",
+                    "required_fields": ["displayname", "password", "email"],
+                }
+            )
     # If not POST
-    return jsonify("This endpoint only supports POST requests for registration.")
+    return jsonify(
+        {"message": "This endpoint only supports POST requests for registration."}
+    )
 
 
 @api.route("/login", methods=["POST"])
@@ -205,24 +195,19 @@ def login():
         if user_obj and check_password_hash(user_obj.Password, password):
             session_instance.close()
 
-            return jsonify({"token": generateToken(user_obj.User_ID)})
+            return jsonify(
+                {
+                    "token": generate_jwt({"User_ID": user_obj.User_ID}),
+                    "message": "Sign in successfull",
+                }
+            )
         else:
             session_instance.close()
-            return "Wrong name or password"
+            return jsonify({"message": "Incorrect email or password!"})
     else:
-        return jsonify("Method was not POST")
-
-
-@api.route("/securedPing")
-@token_required
-def ping(current_user):
-    """
-    A secured route that requires a valid JWT token for access.
-
-    :param current_user: The user context provided by the token_required decorator.
-    :return: JSON response indicating the user who provided the token.
-    """
-    return jsonify("A valid token provided by " + current_user.Name)
+        return jsonify(
+            {"message": "This endpoint only supports POST requests for registration."}
+        )
 
 
 @socketio.on("my_event")
