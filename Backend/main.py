837d57dd847adf754ae9fab26d1ce3393c93882d
Stefi Prokop <hyscript7@seznam.cz>
2023-10-14 04:10:57 +0200

    Perms and Groups APIs (No perm validation)

--- Backend/main.py
+++ Backend/main.py
@@ -6,7 +6,7 @@ import jwt
 from database import Session, func
 from flask import Flask, json, jsonify, request, session
 from flask_socketio import SocketIO
-from models import Users, Groups, Boards, Permissions, Questions, Feedback, Reaction
+from models import Groups, Permissions, Users
 from werkzeug.exceptions import HTTPException
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -48,7 +48,6 @@ def add_header(response):
     :param response: The Flask response object.
     """
     response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Headers"] = "content-type"
     return response
 
 
@@ -157,7 +156,7 @@ def register():
             if not q:
                 usr = Users(
                     Name=name,
-                    Password=generate_password_hash(password),
+                    Password=password,  # generate_password_hash(password),
                     Email=email,
                 )
                 session_instance.add(usr)
@@ -193,7 +192,9 @@ def login():
 
         user_obj = session_instance.query(Users).filter_by(Email=email).first()
 
-        if user_obj and check_password_hash(user_obj.Password, password):
+        if (
+            user_obj and password == user_obj.Password
+        ):  # check_password_hash(user_obj.Password, password):
             session_instance.close()
 
             return jsonify(
@@ -211,22 +212,210 @@ def login():
         )
 
 
+@api.route("/permission/get/", methods=["POST"])
+def perm_get():
+    if request.method == "POST":
+        permid = request.json.get("permid")
+        if not permid:
+            return jsonify(
+                {
+                    "message": "You must provide the required fields!",
+                    "required_fields": ["permid"],
+                }
+            )
+        p = session_instance.query(Permissions).filter_by(
+            Permission_ID=permid).first()
+        if not p:
+            return jsonify({"message": "A permission with this ID does not exist!"})
+        return jsonify(
+            {
+                "message": "Found a permission entry!",
+                "userid": p.User_ID,
+                "groupid": p.Group_ID,
+                "level": p.Permission_Level,
+            }
+        )
+    else:
+        return jsonify(
+            {"message": "This endpoint only supports POST requests for registration."}
+        )
+
+
+@api.route("/permission/create/", methods=["POST"])
+def perm_create():
+    if request.method == "POST":
+        userid = request.json.get("userid")
+        groupid = request.json.get("groupid")
+        level = request.json.get("level")
+        if None in [userid, groupid, level]:
+            return jsonify(
+                {
+                    "message": "You must provide the required fields!",
+                    "required_fields": ["userid", "groupid", "level"],
+                }
+            )
+        if level >= 3 or level < 0:
+            return jsonify({"message": "Level must be in the interval <0;3)"})
+        q_link_exists = (
+            session_instance.query(Permissions)
+            .filter_by(User_ID=userid, Group_ID=groupid)
+            .first()
+        )
+        if q_link_exists:
+            return jsonify(
+                {
+                    "message": "This user is already linked to the specified group! Use update instead!"
+                }
+            )
+        r = Permissions(Permission_Level=level,
+                        Group_ID=groupid, User_ID=userid)
+        session_instance.add(r)
+        session_instance.commit()
+        return jsonify(
+            {"message": "Created a new permission link", "permid": r.Permission_ID}
+        )
+    else:
+        return jsonify(
+            {"message": "This endpoint only supports POST requests for registration."}
+        )
+
+
+@api.route("/permission/delete/", methods=["POST"])
+def perm_delete():
+    if request.method == "POST":
+        permid = request.json.get("permid")
+        if not permid:
+            return jsonify(
+                {
+                    "message": "You must provide the required fields!",
+                    "required_fields": ["permid"],
+                }
+            )
+        q = session_instance.query(Permissions).filter_by(
+            Permission_ID=permid).first()
+        if not q:
+            return jsonify({"message": "Permission link with this ID does not exist"})
+        session_instance.delete(q)
+        return jsonify({"message": "Permission link destroyed"})
+    else:
+        return jsonify(
+            {"message": "This endpoint only supports POST requests for registration."}
+        )
+
+
+@api.route("/permission/update/", methods=["POST"])
+def perm_update():
+    if request.method == "POST":
+        permid = request.json.get("permid")
+        level = request.json.get("level")
+        if None in [permid, level]:
+            return jsonify(
+                {
+                    "message": "You must provide the required fields!",
+                    "required_fields": ["permid", "level"],
+                }
+            )
+        if level >= 3 or level < 0:
+            return jsonify({"message": "Level must be in the interval <0;3)"})
+        q = session_instance.query(Permissions).filter_by(
+            Permission_ID=permid).first()
+        if not q:
+            return jsonify({"message": "Permission link with this ID does not exist"})
+        q.Permission_Level = level
+        session_instance.add(q)
+        session_instance.commit()
+        return jsonify(
+            {"message": "Permission link updated with the new permission level."}
+        )
+    else:
+        return jsonify(
+            {"message": "This endpoint only supports POST requests for registration."}
+        )
+
+
+@api.route("/permission/users_in", methods=["POST"])
+def perm_group_users():
+    if request.method == "POST":
+        groupid = request.json.get("groupid")
+        limit = request.json.get("limit")
+        offset = request.json.get("offset", 0)
+        if None in [groupid, limit, offset]:
+            return jsonify(
+                {
+                    "message": "You must provide the required fields!",
+                    "required_fields": ["groupid", "limit", "offset"],
+                }
+            )
+        q = [
+            i.User_ID
+            for i in (
+                session_instance.query(Permissions)
+                .filter_by(Group_ID=groupid)
+                .limit(limit)
+                .offset(offset * limit)
+            )
+        ]
+        if not q:
+            return jsonify({"message": "This group does not have any users in it"})
+        return jsonify(
+            {
+                "message": f"Found all users ({limit} per page, page {offset+1}) in the specified group",
+                "users": q,
+            }
+        )
+    else:
+        return jsonify(
+            {"message": "This endpoint only supports POST requests for registration."}
+        )
+
+
+@api.route("/permission/groups_of", methods=["POST"])
+def perm_user_groups():
+    if request.method == "POST":
+        userid = request.json.get("userid")
+        limit = request.json.get("limit")
+        offset = request.json.get("offset", 0)
+        if None in [userid, limit, offset]:
+            return jsonify(
+                {
+                    "message": "You must provide the required fields!",
+                    "required_fields": ["userid", "limit", "offset"],
+                }
+            )
+        q = [
+            i.User_ID
+            for i in (
+                session_instance.query(Permissions)
+                .filter_by(User_ID=userid)
+                .limit(limit)
+                .offset(offset * limit)
+            )
+        ]
+        if not q:
+            return jsonify({"message": "This user is not in any groups"})
+        return jsonify(
+            {
+                "message": f"Found all groups ({limit} per page, page {offset+1}) for the specified user",
+                "groups": q,
+            }
+        )
+    else:
+        return jsonify(
+            {"message": "This endpoint only supports POST requests for registration."}
+        )
+
+
 @api.route("/addGroup", methods=["post"])
 def addGroup():
     if request.method == "POST":
-        GroupName = request.json["GroupName"]
+        Group_Name = request.json["Group_Name"]
         Description = request.json["Description"]
 
-        Group_obj = session_instance.query(Groups).filter_by(GroupName=GroupName)
+        GroupToCommit = Groups(Group_Name=Group_Name, Description=Description)
+        session_instance.add(GroupToCommit)
 
-        if GroupName not in Group_obj:
-            GroupToCommit = Groups(
-                GroupName=GroupName, 
-                Description=Description
-            )
-            session_instance.add(GroupToCommit)
-            return jsonify("Group created")
-        return jsonify("You shouldn't get this response")
+        session_instance.commit()
+        return jsonify("Group created")
     return jsonify("Wrong request")
 
 
@@ -235,11 +424,15 @@ def addGroup():
 def removeGroup(u: Users):
     if request.method == "DELETE":
         groupToDelete = request.json.get("Group_ID")
-        perms = session_instance.query(Permissions).filter_by(User_ID=u.User_ID, Group_ID=groupToDelete, Permissions_Level=3)
+        perms = session_instance.query(Permissions).filter_by(
+            User_ID=u.User_ID, Group_ID=groupToDelete, Permissions_Level=3
+        )
         if not perms:
             return jsonify("Fuck off")
-        group_obj = session_instance.query(Groups).filter_by(Group_ID=groupToDelete)
+        group_obj = session_instance.query(
+            Groups).filter_by(Group_ID=groupToDelete)
         session_instance.delete(group_obj)
+        session_instance.commit()
         return jsonify("Group deleted")
     return jsonify("Wrong request")
 
@@ -248,11 +441,8 @@ def removeGroup(u: Users):
 def listGroups():
     if request.method == "GET":
         group_obj = session_instance.query(Groups).all()
-        i = []
-        for group in group_obj.filter_by(Group_ID=group):
-            item = [group.Group_ID, group.Group_Name]
-            i.append(item)
-        return i
+        groups = [[group.Group_ID, group.Group_Name] for group in group_obj]
+        return groups
 
 
 @socketio.on("my_event")
