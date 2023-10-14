a43a533b5d520fe2be08b3194cce55973fa1d964
Jan Palma <60009551+mobilex1122@users.noreply.github.com>
2023-10-14 02:32:33 +0200

    Added null logo alias

---
+++ Frontend/src/routes/nullLogo.svelte
@@ -0,0 +1,18 @@
+<a href="https://projnull.mobilex1122.eu" target="_blank" class="absolute bottom-5">
+	<img class="w-16 sm:w-24 mx-auto" src="https://projnull.mobilex1122.eu/wp-content/uploads/sites/3/2023/10/null-logo_white-big-transparent.png" alt="">
+</a>
+
+<style>
+	a {
+		transform: translate(-50%,-50%);
+		left: 50%;
+	}
+	a img {
+		
+		transform: scale(1);
+		transition: all 200ms;
+	}
+	a:hover img {
+		transform:scale(1.2);
+	}
+</style>
