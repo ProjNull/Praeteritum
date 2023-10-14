03d69e9fd63989148075cdf6dbd9dca214acbd80
Jan Palma <60009551+mobilex1122@users.noreply.github.com>
2023-10-14 02:37:45 +0200

    added board "tribus" render

---
+++ Frontend/src/routes/tribus/table.svelte
@@ -0,0 +1,16 @@
+<script>
+	let tables
+</script>
+
+<div class="bg-gray-300 rounded-xl p-5 w-[20rem]">
+	<h1>Table Name</h1>
+	<div>
+		<div class="bg-gray-400 p-2 rounded-xl">
+			some text
+		</div>
+	</div>
+	<form>
+		<input type="text" name="" id="" placeholder="Text">
+		<input type="submit" value="Add">
+	</form>
+</div>
