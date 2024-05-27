07f1a918773a3b261f6c68e64ea63883487b1818
HyScript7 <hyscript7@seznam.cz>
2024-05-27 14:10:25 +0200

    DB: Refactor and finish DB engine implementation (Read desc.) - Got rid of redundant database.py, moved contents into __init__ instead; Import using: import get_session from .database; Instead of import get_session from .database.database (Why would you even do it this way?) - Added a run script to start the application using the proper database driver for development (sqlite) - Added a primary key to the example model to prevent the application from throwing

--- backend/app/database/models/example_model.py
+++ backend/app/database/models/example_model.py
@@ -1,6 +1,11 @@
-from ..database import Base
-from sqlalchemy import Column, Integer, String, DateTime, Boolean
+from sqlalchemy import Column, Integer
+
+from .. import Base
+
 
 class SusModel(Base):
-    __tablename__ = 'sus'
-    ...
+    __tablename__ = "sus"
+    sus_id: int = Column(Integer, primary_key=True)
+
+    def __init__(self, sus_id: int):
+        self.sus_id = sus_id
---
+++ backend/run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+DB_DRIVER=sqlite python3 -m app
