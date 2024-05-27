ec5a1cdb72bbb7ce937bf47ab6d0e87dde2549bd
HyScript7 <hyscript7@seznam.cz>
2024-05-27 14:53:46 +0200

    DB: Fix revert syntax bs

--- backend/app/database/models/example_model.py
+++ backend/app/database/models/example_model.py
@@ -3,15 +3,9 @@ from sqlalchemy import Column, Integer
 from .. import Base
 
 
-<<<<<<< Updated upstream
 class SusModel(Base):
     __tablename__ = "sus"
     sus_id: int = Column(Integer, primary_key=True)
 
     def __init__(self, sus_id: int):
         self.sus_id = sus_id
-=======
-class sus_model(Base):
-    __tablename__ = 'sus'
-    ...
->>>>>>> Stashed changes
