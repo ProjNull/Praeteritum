9091772b15df9267a59a5ec6d86554636c25b03a
SomeKristi <kmaas2007@gmail.com>
2024-05-28 15:01:29 +0200

    edit db models

--- backend/app/database/models/example_model.py
+++ backend/app/database/models/example_model.py
@@ -5,6 +5,7 @@ from .. import Base
 class UserToGroup(Base):
     __tablename__ = "user_to_group"
     utg_id = Column(Integer, primary_key=True, autoincrement= "auto", nullable=False)
+    permissions = Column(Integer, nullable=False)
     user_id = Column(String, nullable=False)
     group_id = Column(Integer, ForeignKey("groups.group_id"), nullable=False)
 
