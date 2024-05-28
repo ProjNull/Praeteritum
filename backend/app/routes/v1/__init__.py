347a0b1720be7dc81f8b83eef93ffae99b654727
HyScript7 <hyscript7@seznam.cz>
2024-05-28 14:25:55 +0200

    BE: Most bare auth implementation ever seen

--- backend/app/routes/v1/__init__.py
+++ backend/app/routes/v1/__init__.py
@@ -6,3 +6,7 @@ v1_router = APIRouter(prefix="/v1")
 @v1_router.get("/", response_description="Hello, World!", status_code=200)
 async def v1_root():
     return {"message": "Hello, World!"}
+
+from .kinde import kinde_router
+
+v1_router.include_router(kinde_router)
--- backend/app/routes/v1/kinde.py
+++ backend/app/routes/v1/kinde.py
@@ -1,82 +1,93 @@
-from typing import Union
-from fastapi import FastAPI, Depends, HTTPException, status, Request, APIRouter
+from typing import Annotated, Dict
+
+from fastapi import (
+    APIRouter,
+    Depends,
+    Header,
+    HTTPException,
+    Request,
+    status,
+)
 from fastapi.responses import RedirectResponse
-from starlette.middleware.sessions import SessionMiddleware
 from kinde_sdk import Configuration
-from kinde_sdk.kinde_api_client import KindeApiClient, GrantType
-import config
+from kinde_sdk.kinde_api_client import KindeApiClient
+from fastapi.security import HTTPBearer
+
+from ...config import (
+    GRANT_TYPE,
+    KINDE_CLIENT_ID,
+    KINDE_CLIENT_SECRET,
+    KINDE_HOST,
+    KINDE_REDIRECT_URL,
+)
+
+configuration = Configuration(host=KINDE_HOST)
+kinde_api_client_params = {
+    "configuration": configuration,
+    "domain": KINDE_HOST,
+    "client_id": KINDE_CLIENT_ID,
+    "client_secret": KINDE_CLIENT_SECRET,
+    "grant_type": GRANT_TYPE,  # client_credentials | authorization_code | authorization_code_with_pkce
+    "callback_url": KINDE_REDIRECT_URL,
+}
 
 kinde_router = APIRouter(prefix="/kinde")
 
-kinde_router.add_middleware(SessionMiddleware, secret_key=config.SECRET_KEY)
+user_clients: Dict[str, KindeApiClient] = {}
 
-# User clients dictionary to store Kinde clients for each user
-user_clients = {}
 
-# Initialize Kinde client with configuration
-configuration = Configuration(host=config.KINDE_ISSUER_URL)
-kinde_api_client_params = {
-    "configuration": configuration,
-    "domain": config.KINDE_ISSUER_URL,
-    "client_id": config.CLIENT_ID,
-    "client_secret": config.CLIENT_SECRET,
-    "grant_type": config.GRANT_TYPE,
-    "callback_url": config.KINDE_CALLBACK_URL,
-}
-if config.GRANT_TYPE == GrantType.AUTHORIZATION_CODE_WITH_PKCE:
-    kinde_api_client_params["code_verifier"] = config.CODE_VERIFIER
-
-# User clients dictionary to store Kinde clients for each user
-user_clients = {}
-
-# Dependency to get the current user's KindeApiClient instance
-def get_kinde_client(request: Request) -> KindeApiClient:
-    user_id = request.session.get("user_id")
+def get_kinde_client(user_id: Annotated[str, Header()]) -> KindeApiClient:
     if user_id is None:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authenticated")
-
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authenticated"
+        )
     if user_id not in user_clients:
         # If the client does not exist, create a new instance with parameters
         user_clients[user_id] = KindeApiClient(**kinde_api_client_params)
-
     kinde_client = user_clients[user_id]
     # Ensure the client is authenticated
     if not kinde_client.is_authenticated():
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authenticated")
-
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authenticated"
+        )
     return kinde_client
 
-# Login endpoint
-@kinde_router.get("/api/auth/login")
-def login(request: Request):
+
+@kinde_router.get("/me")
+async def me(kinde_client=Depends(get_kinde_client)):
+    return kinde_client.get_user_details()
+
+
+@kinde_router.get("/login")
+def login():
     kinde_client = KindeApiClient(**kinde_api_client_params)
     login_url = kinde_client.get_login_url()
     return RedirectResponse(login_url)
 
+
 # Register endpoint
-@kinde_router.get("/api/auth/register")
-def register(request: Request):
+@kinde_router.get("/register")
+def register():
     kinde_client = KindeApiClient(**kinde_api_client_params)
     register_url = kinde_client.get_register_url()
     return RedirectResponse(register_url)
 
-@kinde_router.get("/api/auth/kinde_callback")
+
+@kinde_router.get("/callback")
 def callback(request: Request):
     kinde_client = KindeApiClient(**kinde_api_client_params)
     kinde_client.fetch_token(authorization_response=str(request.url))
     user = kinde_client.get_user_details()
-    request.session["user_id"] = user.get("id")
     user_clients[user.get("id")] = kinde_client
-    return RedirectResponse(kinde_router.url_path_for("read_root"))
+    return {"Your id":user.get("id")}
 
-# Logout endpoint
-@kinde_router.get("/api/auth/logout")
-def logout(request: Request):
-    user_id = request.session.get("user_id")
+
+@kinde_router.get("/logout")
+def logout(user_id: Annotated[str, Header()]):
     if user_id in user_clients:
         kinde_client = user_clients[user_id]
-        logout_url = kinde_client.logout(redirect_to=config.LOGOUT_REDIRECT_URL)
-        del user_clients[user_id]
-        request.session.pop("user_id", None)
+        logout_url = kinde_client.logout(redirect_to="/api/v1/")
         return RedirectResponse(logout_url)
-    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authenticated")
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authenticated"
+    )
