fba2876cc4d0ea28722619412bc0cee710946d1c
HyScript7 <hyscript7@seznam.cz>
2024-05-29 13:39:34 +0200

    DB: Update retro model

--- backend/app/database/models/retros_model.py
+++ backend/app/database/models/retros_model.py
@@ -8,10 +8,14 @@ class Retros(Base):
     group_id = Column(Integer, ForeignKey("groups.group_id"), nullable=False)
     user_id = Column(String, nullable=False)
     name = Column(String(64), nullable=False)
+    desc = Column(String(255), nullable=False)
     stage = Column(Integer, default=0)
-    ended = Column(Boolean, default=False)
+    is_active = Column(Boolean, default=True)
+    is_public = Column(Boolean, default=False)
 
-    def __init__(self, group_id, user_id, name):
+    def __init__(self, group_id, user_id, name, desc: str = "A retro...", is_public: bool = False):
         self.group_id = group_id
         self.user_id = user_id
         self.name = name
+        self.desc = desc
+        self.is_public = is_public
