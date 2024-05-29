c336c6a19b89fcfc191c549b41e80191958f2703
HyScript7 <hyscript7@seznam.cz>
2024-05-29 13:48:44 +0200

    DB: Update models (Wth?)

--- backend/app/database/models/votes_model.py
+++ backend/app/database/models/votes_model.py
@@ -4,6 +4,7 @@ from .. import Base
 
 class Votes(Base):
     __tablename__ = "votes"
+    vote_id = Column(Integer, primary_key=True, autoincrement= "auto", nullable=False)
     retro_id = Column(Integer, ForeignKey("retros.retro_id"), nullable=False)
     note_id = Column(Integer, ForeignKey("notes.note_id"), nullable=False)
 
