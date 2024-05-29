c4c5cac27543969bdda8e4581e0461f4182c4630
Kaiy <106684837+KaiyWhiteSnow@users.noreply.github.com>
2024-05-29 14:56:38 +0200

    BE: Quick fix

--- backend/app/routes/v1/teams.py
+++ backend/app/routes/v1/teams.py
@@ -9,41 +9,41 @@ def get_all_teams_in_org(body: team_service.team_schemas.QueryAllTeamsInOrganiza
     return team_service.get_all_teams_in_org(db, body)
 
 @teams_router.get("/get_team_by_id")
-def get_team_by_id(body: team_service.team_schemas.QueryAllTeamsInOrganization, db = Depends(get_session)):
+def get_team_by_id(body: team_service.team_schemas.QueryTeam, db = Depends(get_session)):
     return team_service.get_team_by_id(db, body)
 
 @teams_router.get("/get_all_team_member_relations")
-def get_all_team_member_relations(body: team_service.team_schemas.QueryAllTeamsInOrganization, db = Depends(get_session)):
+def get_all_team_member_relations(body: team_service.team_schemas.QueryTeam, db = Depends(get_session)):
     return team_service.get_all_team_member_relations(db, body)
 
 @teams_router.get("/get_team_member_relation_by_id")
-def get_team_member_relation_by_idg(body: team_service.team_schemas.QueryAllTeamsInOrganization, db = Depends(get_session)):
+def get_team_member_relation_by_idg(body: team_service.team_schemas.QueryTeamMember, db = Depends(get_session)):
     return team_service.get_team_member_relation_by_id(db, body)
 
 @teams_router.get("/get_user_teams")
-def get_user_teams(body: team_service.team_schemas.QueryAllTeamsInOrganization, db = Depends(get_session)):
+def get_user_teams(body: team_service.team_schemas.QueryAllUsersTeams, db = Depends(get_session)):
     return team_service.get_user_teams(db, body)
 
 @teams_router.post("/get_user_permission_level_in_team")
-def get_user_permission_level_in_team(body: team_service.team_schemas.QueryAllTeamsInOrganization, db = Depends(get_session)):
+def get_user_permission_level_in_team(body: team_service.team_schemas.TeamMember, db = Depends(get_session)):
     return team_service.get_user_permission_level_in_team(db, body)
 
 @teams_router.post("/set_user_permission_level_in_team")
-def set_user_permission_level_in_team(body: team_service.team_schemas.QueryAllTeamsInOrganization, db = Depends(get_session)):
+def set_user_permission_level_in_team(body: team_service.team_schemas.TeamMemberUpdate, db = Depends(get_session)):
     return team_service.set_user_permission_level_in_team(db, body)
 
 @teams_router.post("/add_user_to_team")
-def add_user_to_team(body: team_service.team_schemas.QueryAllTeamsInOrganization, db = Depends(get_session)):
+def add_user_to_team(body: team_service.team_schemas.TeamMemberBase, db = Depends(get_session)):
     return team_service.add_user_to_team(db, body)
 
 @teams_router.post("/remove_user_from_team")
-def remove_user_from_team(body: team_service.team_schemas.QueryAllTeamsInOrganization, db = Depends(get_session)):
+def remove_user_from_team(body: team_service.team_schemas.TeamMemberBase, db = Depends(get_session)):
     return team_service.remove_user_from_team(db, body)
 
 @teams_router.post("/create_team")
-def create_team(body: team_service.team_schemas.QueryAllTeamsInOrganization, db = Depends(get_session)):
+def create_team(body: team_service.team_schemas.TeamBase, db = Depends(get_session)):
     return team_service.create_team(db, body)
 
 @teams_router.post("/delete_team")
-def delete_team(body: team_service.team_schemas.QueryAllTeamsInOrganization, db = Depends(get_session)):
+def delete_team(body: team_service.team_schemas.TeamBase, db = Depends(get_session)):
     return team_service.delete_team(db, body)
