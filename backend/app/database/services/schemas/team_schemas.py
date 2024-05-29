e23008847e1a6f08ce37b4b4b97b4ee171646771
HyScript7 <hyscript7@seznam.cz>
2024-05-29 09:54:48 +0200

    BE: Define team schemas for queries

---
+++ backend/app/database/services/schemas/team_schemas.py
@@ -0,0 +1,49 @@
+from pydantic import BaseModel, Field
+
+class TeamBase(BaseModel):
+    name: str
+
+class TeamCreate(TeamBase):
+    pass
+
+class TeamUpdate(TeamBase):
+    pass
+
+class Team(TeamBase):
+    team_id: int
+    organization_id: int
+
+    class Config:
+        orm_mode = True
+
+class TeamMemberBase(BaseModel):
+    team_id: int
+    user_id: str
+
+class TeamMemberUpdate(TeamMemberBase):
+    permission_level: int = Field(default=1)
+
+class TeamMemberCreate(TeamMemberBase, TeamMemberUpdate):
+    pass
+
+class TeamMember(TeamMemberBase):
+    utt_id: int
+    permission_level: int
+
+    class Config:
+        orm_mode = True
+
+class QueryAllTeamsInOrganization(BaseModel):
+    organization_id: int
+
+class QueryTeam(BaseModel):
+    team_id: int
+
+class QueryAllUsersTeams(BaseModel):
+    user_id: str
+
+class QueryMembersInTeam(QueryTeam):
+    team_id: int
+
+class QueryTeamMember(TeamMemberBase):
+    pass
