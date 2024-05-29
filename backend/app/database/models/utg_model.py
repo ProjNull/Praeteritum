7ad519541a0a9564b29ec8dda87f546c683354dc
SomeKristi <kmaas2007@gmail.com>
2024-05-29 13:27:31 +0200

    updated db models

--- backend/app/database/models/utg_model.py
+++ backend/app/database/models/utg_model.py
@@ -5,13 +5,15 @@ from .. import Base
 class UserToGroup(Base):
     __tablename__ = "user_to_group"
     utg_id = Column(Integer, primary_key=True, autoincrement= "auto", nullable=False)
-    permissions = Column(Integer, nullable=False)
+    permissions = Column(Integer, default=0)
     user_id = Column(String, nullable=False)
     group_id = Column(Integer, ForeignKey("groups.group_id"), nullable=False)
 
-    def __init__(self, user_id, group_id):
+    def __init__(self, user_id, group_id, permissions: int | None):
         self.user_id = user_id
         self.group_id = group_id
+        if permissions is not None:
+            self.permissions = permissions
 
 class Invites(Base):
     __tablename__ = "invites"
