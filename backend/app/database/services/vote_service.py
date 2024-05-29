72380e15ac6c0fac2ba3ac84385771a156a52a2f
SomeKristi <kmaas2007@gmail.com>
2024-05-29 22:26:25 +0200

    Update vote_service.py

--- backend/app/database/services/vote_service.py
+++ backend/app/database/services/vote_service.py
@@ -1,12 +1,16 @@
 from ..models.votes_model import Votes
 from ..models.utr_model import UserToRetro
-from ..models.utg_model import UserToGroup
-from ..models.retros_model import Retros
+from ..models.notes_model import Notes
 from .. import Session
 from fastapi import HTTPException
 
-async def add_vote(db: Session, retro_id: int, user_id: str):
-    db.add(Votes(retro_id=retro_id, note_id=))
-async def get_vote_count(db: Session, user_id: str, retro_id: int):
+async def add_vote(db: Session, user_id: str, note_id: int):
+    if db.query(UserToRetro).filter(UserToRetro.user_id==user_id and UserToRetro.retro_id == db.query(Notes).filter(Notes.note_id==note_id).first().retro_id).first() is not None:
+        db.add(Votes(user_id=user_id, note_id=note_id))
+    else:
+        raise (HTTPException("User is not a member of this retro"))
+
+async def get_vote_count(db: Session, user_id: str):
+    
     #return the number of votes in a retro
-    pass
+    ...
