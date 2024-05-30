336f6d03907301a3473da3ec5b56602e5c747657
SomeKristi <kmaas2007@gmail.com>
2024-05-30 22:22:42 +0200

    schematized action services

--- backend/app/database/services/action_service.py
+++ backend/app/database/services/action_service.py
@@ -1,29 +1,34 @@
 from typing import List
 from ..models.actions_model import Actions
 from ..models.uta_model import UserToAction
-from ..models.retros_model import Retros
+from .schemas.retro_schemas import QueryAllRetrosInOrganization
+from .retro_service import get_all_retros_in_org
+from .schemas import action_schemas
 from .. import Session
 from fastapi import HTTPException
 
-async def create_action(db: Session, user_id: str, retro_id: int, name: str, description: str):
+async def create_action(db: Session, query: action_schemas.CreateAction):
     #add permissions checking
-    db.add(Actions(retro_id, name, description))
+    db.add(Actions(query.retro_id, query.name, query.description))
 
-async def asign_users_to_action(db: Session, user_ids: List[str], action_id: int):
-    for user_id in user_ids:
-        db.add(UserToAction(user_id, action_id))
-    
-async def remove_users_from_action(db: Session, user_ids: List[str], action_id: int):
-    db.query(UserToAction).filter(UserToAction.user_id in user_ids and UserToAction.action_id == action_id).delete()
-    
-async def remove_action(db: Session, user_id: str, action_id: int):
+async def asign_users_to_action(db: Session, query: action_schemas.AsignUsersToAction):
     #add permissions checking
-    db.query(Actions).filter(Actions.action_id == action_id).delete()
+    for user_id in query.user_ids:
+        db.add(UserToAction(user_id, query.action_id))
+    
+async def remove_users_from_action(db: Session, query: action_schemas.removeUsersFromAction):
+    #add permissions checking
+    db.query(UserToAction).filter(UserToAction.user_id in query.user_ids and UserToAction.action_id == query.action_id).delete()
+    
+async def remove_action(db: Session, query: action_schemas.RemoveAction):
+    #add permissions checking
+    db.query(Actions).filter(Actions.action_id == query.action_id).delete()
 
-async def get_actions_for_group(db: Session, user_id: str, group_id: int):
-    return db.query(Actions).filter(Actions.retro_id in retro_service.get_retros_for_org()).all()
-    #get actions in a group
-    ...
-async def get_actions_for_user(db: Session, user_id: str):
-    #get actions belonging to a user
-    ...
+async def get_actions_for_group(db: Session, query: action_schemas.GetActionsForGroup):
+    #add permissions checking
+    retro_ids = (retro.retro_id for retro in get_all_retros_in_org(db, QueryAllRetrosInOrganization(query.group_id)))
+    return db.query(Actions).filter(Actions.retro_id in retro_ids).all()
+
+async def get_actions_for_user(db: Session, query: action_schemas.GetActionsForUser) -> List[Actions]:
+    action_ids = (action.action_id for action in db.query(UserToAction).filter(UserToAction.user_id == query.user_id).all())
+    return db.query(Actions).filter(Actions.action_id in action_ids).all()
---
+++ backend/app/database/services/schemas/action_schemas.py
@@ -0,0 +1,38 @@
+from typing import List
+from pydantic import BaseModel, Field
+
+class ActionBase(BaseModel):
+    retro_id: int
+    name: str
+    description: str = Field(default="An action...")
+
+class Action(ActionBase):
+    action_id: int
+    retro_id: int
+    name: str
+    description: str
+
+class CreateAction(BaseModel):
+    user_id: str
+    retro_id: int
+    name: str
+    description: str
+
+class AsignUsersToAction(BaseModel):
+    user_ids: List[str]
+    action_id: int
+
+class removeUsersFromAction(BaseModel):
+    user_ids: List[str]
+    action_id: int
+
+class RemoveAction(BaseModel):
+    user_id: str
+    action_id: int
+
+class GetActionsForGroup(BaseModel):
+    user_id: str
+    group_id: int
+
+class GetActionsForUser(BaseModel):
+    user_id: str
