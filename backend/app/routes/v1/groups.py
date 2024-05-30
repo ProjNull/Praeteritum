6ca52820224324deb98676327b7635f3ae0008df
Kaiy <106684837+KaiyWhiteSnow@users.noreply.github.com>
2024-05-30 12:49:22 +0200

    BE: Fixed types, boards ready to be implemented

--- backend/app/routes/v1/groups.py
+++ backend/app/routes/v1/groups.py
@@ -4,7 +4,7 @@ from ...database import get_session
 
 groups_router = APIRouter(prefix="/groups")
 
-@groups_router.get("/invite_user")
+@groups_router.post("/invite_user")
 def invite_user(body: group_service.group_schemas.InviteUser, db = Depends(get_session)):
     return group_service.invite_user(db, body)
 
@@ -12,19 +12,19 @@ def invite_user(body: group_service.group_schemas.InviteUser, db = Depends(get_s
 def join_group(body: group_service.group_schemas.JoinGroup, db = Depends(get_session)):
     return group_service.join_group(db, body)
 
-@groups_router.get("/leave_group")
+@groups_router.delete("/leave_group")
 def leave_group(body: group_service.group_schemas.LeaveGroup, db = Depends(get_session)):
     return group_service.leave_group(db, body)
 
-@groups_router.get("/create_group")
+@groups_router.post("/create_group")
 def create_group(body: group_service.group_schemas.CreateGroup, db = Depends(get_session)):
     return group_service.create_group(db, body)
 
-@groups_router.get("/set_owner")
+@groups_router.patch("/set_owner")
 def set_owner(body: group_service.group_schemas.SetOwner, db = Depends(get_session)):
     return group_service.set_owner(db, body)
 
-@groups_router.get("/delete_group")
+@groups_router.delete("/delete_group")
 def delete_group(body: group_service.group_schemas.DeleteGroup, db = Depends(get_session)):
     return group_service.delete_group(db, body)
 
--- backend/app/routes/v1/retrospectives.py
+++ backend/app/routes/v1/retrospectives.py
@@ -4,7 +4,7 @@ from ...database import get_session
 
 retrospectives_router = APIRouter(prefix="/retrospectives")
 
-@retrospectives_router.get("/create_retro")
+@retrospectives_router.post("/create_retro")
 def create_retro(body: retro_service.retro_schemas.RetroCreate, db = Depends(get_session)):
     return retro_service.create_retro(db, body)
 
@@ -24,11 +24,11 @@ def get_all_retros_by_user(body: retro_service.retro_schemas.QueryAllRetrosForUs
 def fetch_retros(body: retro_service.retro_schemas.QueryRetroList, db = Depends(get_session)):
     return retro_service.fetch_retros(db, body)
 
-@retrospectives_router.get("/delete_retro")
+@retrospectives_router.delete("/delete_retro")
 def delete_retro(body: retro_service.retro_schemas.QueryRetro, db = Depends(get_session)):
     return retro_service.delete_retro(db, body)
 
-@retrospectives_router.get("/update_retro")
+@retrospectives_router.patch("/update_retro")
 def update_retro(body: retro_service.retro_schemas.RetroUpdate, db = Depends(get_session)):
     return retro_service.update_retro(db, body)
 
@@ -36,10 +36,10 @@ def update_retro(body: retro_service.retro_schemas.RetroUpdate, db = Depends(get
 def get_retro_members(body: retro_service.retro_schemas.QueryRetro, db = Depends(get_session)):
     return retro_service.get_retro_members(db, body)
 
-@retrospectives_router.get("/add_user_to_retro")
+@retrospectives_router.post("/add_user_to_retro")
 def add_user_to_retro(body: retro_service.retro_schemas.UserToRetro, db = Depends(get_session)):
     return retro_service.add_user_to_retro(db, body)
 
-@retrospectives_router.get("/remove_user_from_retro")
+@retrospectives_router.delete("/remove_user_from_retro")
 def remove_user_from_retro(body: retro_service.retro_schemas.UserToRetro, db = Depends(get_session)):
     return retro_service.remove_user_from_retro(db, body)
