2b9f5942b20b8ea47784c23b81c29c592d22345f
Kaiy <106684837+KaiyWhiteSnow@users.noreply.github.com>
2024-05-31 08:49:03 +0200

    BE: I have no clue why this wasn't commited

---
+++ backend/app/routes/v1/notes.py
@@ -0,0 +1,22 @@
+from fastapi import Depends, Request, APIRouter
+from ...database.services import note_service
+from ...database import get_session
+
+notes_router = APIRouter(prefix="/notes")
+
+@notes_router.post("/add_note")
+def add_note(body: note_service.note_schemas.AddNote, db = Depends(get_session)):
+    return note_service.add_note(db, body)
+
+@notes_router.post("/move_note")
+def move_note(body: note_service.note_schemas.MoveNote, db = Depends(get_session)):
+    return note_service.move_note(db, body)
+
+@notes_router.post("/remove_note")
+def remove_note(body: note_service.note_schemas.RemoveNote, db = Depends(get_session)):
+    return note_service.remove_note(db, body)
+
+@notes_router.post("/get_notes")
+def get_notes(body: note_service.note_schemas.GetNotes, db = Depends(get_session)):
+    return note_service.get_notes(db, body)
+
