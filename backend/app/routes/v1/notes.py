f7c636bba8dd956b7abe19e0afe3d2f0a09b26cb
SomeKristi <kmaas2007@gmail.com>
2024-06-04 11:27:45 +0200

    Note services routes improved

--- backend/app/routes/v1/notes.py
+++ backend/app/routes/v1/notes.py
@@ -7,17 +7,21 @@ notes_router = APIRouter(prefix="/notes")
 
 @notes_router.post("/add_note")
 async def add_note(body: note_service.note_schemas.AddNote, db = Depends(get_session), kinde_client=Depends(user_service.get_kinde_client)):
-    return await note_service.add_note(db, body)
+    user_id = kinde_client.get_user_details().get("id")
+    return await note_service.add_note(db, body, user_id)
 
 @notes_router.post("/move_note")
 async def move_note(body: note_service.note_schemas.MoveNote, db = Depends(get_session), kinde_client=Depends(user_service.get_kinde_client)):
-    return await note_service.move_note(db, body)
+    user_id = kinde_client.get_user_details().get("id")
+    return await note_service.move_note(db, body, user_id)
 
 @notes_router.delete("/remove_note")
 async def remove_note(body: note_service.note_schemas.RemoveNote, db = Depends(get_session), kinde_client=Depends(user_service.get_kinde_client)):
-    return await note_service.remove_note(db, body)
+    user_id = kinde_client.get_user_details().get("id")
+    return await note_service.remove_note(db, body, user_id)
 
 @notes_router.post("/get_notes")
 async def get_notes(body: note_service.note_schemas.GetNotes, db = Depends(get_session), kinde_client=Depends(user_service.get_kinde_client)):
-    return await note_service.get_notes(db, body)
+    user_id = kinde_client.get_user_details().get("id")
+    return await note_service.get_notes(db, body, user_id)
 
