11adaec866c3d05c65050d1ad38c0676c6ec182c
SomeKristi <kmaas2007@gmail.com>
2024-06-04 08:53:21 +0200

    Action services update

--- backend/app/database/services/action_service.py
+++ backend/app/database/services/action_service.py
@@ -10,35 +10,52 @@ from fastapi import HTTPException
 
 async def create_action(db: Session, query: action_schemas.CreateAction):
     group_id: int = db.query(Retros.group_id).filter(Retros.retro_id==query.retro_id).first()
-    if db.query(UserToGroup).filter(UserToGroup.user_id==query.user_id and UserToGroup.group_id==group_id).first().permissions > 1:
-        db.add(Actions(query.retro_id, query.name, query.description))
+    relation = db.query(UserToGroup).filter(UserToGroup.user_id==query.user_id and UserToGroup.group_id==group_id).first()
+    if relation is not None:
+        if relation.permissions > 1:
+            db.add(Actions(query.retro_id, query.name, query.description))
+        else:
+            raise HTTPException("User is not permited to create an action")
     else:
-        raise HTTPException("User is not permited to create an action")
+        raise HTTPException("User is not in this group")
 
 async def asign_users_to_action(db: Session, query: action_schemas.AsignUsersToAction):
     group_id: int = db.query(Retros.group_id).join(Actions, Actions.retro_id == Retros.retro_id).filter(Actions.action_id==query.action_id).first()
-    if db.query(UserToGroup).filter(UserToGroup.user_id==query.user_id and UserToGroup.group_id==group_id).first().permissions > 1:
-        for user_id in query.user_ids:
-            db.add(UserToAction(user_id, query.action_id))
+    relation = db.query(UserToGroup).filter(UserToGroup.user_id==query.user_id and UserToGroup.group_id==group_id).first()
+    if relation is not None:
+        if relation.permissions > 1:
+            for user_id in query.user_ids:
+                db.add(UserToAction(user_id, query.action_id))
+        else:
+            raise HTTPException("User is not permited to asign users to this action")
     else:
-        raise HTTPException("User is not permited to asign users to this action")
+        raise HTTPException("User does not have access to this action")
 
 async def remove_users_from_action(db: Session, query: action_schemas.removeUsersFromAction):
     group_id: int = db.query(Retros.group_id).join(Actions, Actions.retro_id == Retros.retro_id).filter(Actions.action_id==query.action_id).first()
-    if db.query(UserToGroup).filter(UserToGroup.user_id==query.user_id, UserToGroup.group_id==group_id).first().permissions > 1:
-        db.query(UserToAction).filter(UserToAction.user_id in query.user_ids and UserToAction.action_id == query.action_id).delete()
+    relation =db.query(UserToGroup).filter(UserToGroup.user_id==query.user_id, UserToGroup.group_id==group_id).first()
+    if relation is not None:
+        if relation.permissions > 1:
+            db.query(UserToAction).filter(UserToAction.user_id in query.user_ids and UserToAction.action_id == query.action_id).delete()
+        else:
+            raise HTTPException("User is not permited to remove users from this action")
     else:
-        raise HTTPException("User is not permited to remove users from this action")
+        raise HTTPException("User does not have access to this action")
         
 async def delete_action(db: Session, query: action_schemas.DeleteAction):
     group_id: int = db.query(Retros.group_id).join(Actions, Actions.retro_id == Retros.retro_id).filter(Actions.action_id==query.action_id).first()
-    if db.query(UserToGroup).filter(UserToGroup.user_id==query.user_id and UserToGroup.group_id==group_id).first().permissions > 1:
-        db.query(Actions).filter(Actions.action_id == query.action_id).delete()
+    relation =db.query(UserToGroup).filter(UserToGroup.user_id==query.user_id, UserToGroup.group_id==group_id).first()
+    if relation is not None:
+        if relation.permissions > 1:
+            db.query(Actions).filter(Actions.action_id == query.action_id).delete()
+        else:
+            raise HTTPException("User is not permited to remove this action")
     else:
-        raise HTTPException("User is not permited to remove this action")
+        raise HTTPException("User does not have access to this action")
 
 async def get_actions_for_group(db: Session, query: action_schemas.GetActionsForGroup) -> list[Actions]:
-    if db.query(UserToGroup).filter(UserToGroup.user_id==query.user_id and UserToGroup.group_id==query.group_id).first() is not None:
+    relation = db.query(UserToGroup).filter(UserToGroup.user_id==query.user_id and UserToGroup.group_id==query.group_id).first()
+    if relation is not None:
         return db.query(Actions).join(Retros, Actions.retro_id == Retros.retro_id).filter(Retros.group_id == query.group_id)
     else:
         raise HTTPException("User is not in this group")
