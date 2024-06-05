ed15eaaad16a2b16b4930382b5518f418ff3df7a
HyScript7 <hyscript7@seznam.cz>
2024-06-05 09:06:53 +0200

    FE: Add more themes than we will have users

--- frontend/src/components/Settings.tsx
+++ frontend/src/components/Settings.tsx
@@ -15,10 +15,23 @@ const SettingsModal: Component<Props> = ({ setter, getter }) => {
             <div class="card w-96 bg-base-100 shadow-xl">
               <div class="card-body">
                 <h2 class="card-title">Application Settings</h2>
-                <div class="flex flex-row gap-2 flex-wrap w-full">
-                  <button class="btn btn-outline grow" onclick={clearTheme}>Auto</button>
-                  <button class="btn btn-outline grow" onclick={() => setTheme("dark")}>Dark</button>
-                  <button class="btn btn-outline grow" onclick={() => setTheme("light")}>Light</button>
+                <div class="flex flex-col flex-wrap gap-2">
+                  <div class="flex flex-row gap-2 flex-wrap w-full">
+                    <button class="btn btn-outline grow" onclick={clearTheme}>Auto</button>
+                    <button class="btn btn-outline grow" onclick={() => setTheme("dark")}>Dark</button>
+                    <button class="btn btn-outline grow" onclick={() => setTheme("light")}>Light</button>
+                  </div>
+                  <div class="flex flex-row gap-2 flex-wrap w-full">
+                    <button class="btn btn-outline grow" onclick={() => setTheme("night")}>Night</button>
+                    <button class="btn btn-outline grow" onclick={() => setTheme("dracula")}>Dracula</button>
+                    <button class="btn btn-outline grow" onclick={() => setTheme("forest")}>Forest</button>
+                    <button class="btn btn-outline grow" onclick={() => setTheme("coffee")}>Coffee</button>
+                  </div>
+                  <div class="flex flex-row gap-2 flex-wrap w-full">
+                    <button class="btn btn-outline grow" onclick={() => setTheme("lemonade")}>Lemonade</button>
+                    <button class="btn btn-outline grow" onclick={() => setTheme("nord")}>Nord</button>
+                    <button class="btn btn-outline grow" onclick={() => setTheme("autumn")}>Autumn</button>
+                  </div>
                 </div>
                 <div class="card-actions justify-end">
                   <button class="btn btn-outline" onClick={() => setter(false)}>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,22 @@
 import daisyui from "daisyui";
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [daisyui],
-}
+  daisyui: {
+    themes: [
+      "light",
+      "dark",
+      "night",
+      "dracula",
+      "forest",
+      "coffee",
+      "lemonade",
+      "nord",
+      "autumn",
+    ],
+  },
+};
