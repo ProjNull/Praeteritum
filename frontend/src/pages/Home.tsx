242d6f6f5ffc51de8cf520006e9ba27829dadad4
HyScript7 <hyscript7@seznam.cz>
2024-06-07 08:53:31 +0200

    FE: Update retro card on the home screen

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -29,56 +29,16 @@ const RetroGroup: Component<RetroGroupProps> = (props) => {
       <h2 class="text-2xl">{props.title}</h2>
       <ul class="list-none flex flex-row flex-wrap gap-2">
         {props.retros.map((retro) => (
-          <li class="border border-white rounded-md p-2">
-            <A href={`/retro/${retro.retro_id}`}>
-              <div class="flex flex-col">
-                <div class="flex flex-row">
-                  <div class="inline">
-                    <span class="text-lg">{retro.name}</span>{" "}
-                    <span class="badge">{"Phase " + retro.stage}</span>{" "}
-                  </div>
-                  <div>
-                    {!retro.is_public ? (
-                      <span>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke-width={1.5}
-                          stroke="currentColor"
-                          class="size-6"
-                        >
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"
-                          />
-                        </svg>
-                      </span>
-                    ) : (
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width={1.5}
-                        stroke="currentColor"
-                        class="size-6"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M13.5 10.5V6.75a4.5 4.5 0 1 1 9 0v3.75M3.75 21.75h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H3.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"
-                        />
-                      </svg>
-                    )}
-                  </div>
-                </div>
-                <div>
+          <li>
+              <div class="card w-96 bg-base-100 shadow-xl">
+                <div class="card-body">
+                  <h2 class="card-title">{retro.name}</h2><div class={"badge " + (retro.is_public ? "badge-success" : "badge-error")}>{retro.is_public ? "Public" : "Private"}</div>
                   <p>{retro.desc}</p>
-                  <p class="italic text-sm">Authored by {retro.user_id}</p>
+                  <div class="card-actions justify-end">
+                    <button class="btn btn-primary" onclick={() => { window.location.href = `/retro/${retro.retro_id}` }}>Open</button>
+                  </div>
                 </div>
               </div>
-            </A>
           </li>
         ))}
       </ul>
